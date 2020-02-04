5209ff3bcd200d5481e788af957467e1437839ca
Bonanov <38221096+bonanov@users.noreply.github.com>
2020-02-04 12:50:06 +0300

    remove even more christmas :(

--- ts/posts/smile-box.tsx
+++ ts/posts/smile-box.tsx
@@ -13,7 +13,7 @@ const thingSmiles = new Set(
   `
   thumbs_up cookie champagne gift hat_santa whiskey ears ears2 hat_blue
   scarf hat_red hat_red2 horns hand_up hand_up2 thumbs_up shovel hat2
-  confetti hat_green cocktail heart heart2 heart3 heart4 cigarette cigarette2 snowman scarf2 sweater
+  confetti hat_green cocktail heart heart2 heart3 heart4 cigarette cigarette2 snowman scarf2 sweater eggplant
   sweater2 fire tophat wine coffee cola chips goose nogoose gun gun2 karandash knife
   ovsyanka ramyun umbrella chocolate mug mug2 snow space mandarin tree
 `
